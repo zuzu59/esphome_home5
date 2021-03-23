921171da4f6c8ad63ddc6aaa6de9dcd0fa3cc71e
Christian Zufferey <christian@zufferey.com>
2021-03-23 10:33:54 +0100

    Ajouté le petit script pour mes alias persos et passé à 30 secondes la veille de th0_mini_d1_ant avec Li-Po

---
+++ alias.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#Petit script pour charger mes alias qui vont bien pour travailler
+#zf210322.1255
+
+echo -e "Il faut faire un 
+
+source ./alias.sh
+
+"
+
+source <(curl -s https://raw.githubusercontent.com/zuzu59/deploy-proxmox/master/alias)
+
+curl -s https://raw.githubusercontent.com/zuzu59/deploy-proxmox/master/alias
+
